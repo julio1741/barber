3c929179f57680cda6f44ae844b6abdef00abf66
Julio Baptista <julio.baptista@routinguc.com>
2022-11-23 21:57:35 -0300

    Add cancel reservation email}

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -12,4 +12,15 @@ class ReservationMailer < ApplicationMailer
     app_host = ENV.fetch('APP_HOST', nil)
     mail(to: @user.email, subject: 'Gracias por reservar!')
   end
+
+  def cancel
+    @reservation = params[:reservation]
+    @user = @reservation.try(:user)
+    email = @user.nil? ? @reservation.email : @user.try(:email)
+    @worker = @reservation.worker
+    @work_day = @reservation.work_day
+    @block_time = @reservation.block_time
+    app_host = ENV.fetch('APP_HOST', nil)
+    mail(to: email, subject: 'Gracias por reservar!') if email.present?
+  end
 end
