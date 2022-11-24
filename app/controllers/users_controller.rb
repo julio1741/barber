4eb3e49c1ac1489000e7dd42efc0e48c6c2b2b97
Julio Baptista <julio.baptista@routinguc.com>
2022-11-24 12:04:18 -0300

    Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApiController
-  before_action :authorize_request, except: :create
+  before_action :authorize_request, except: [:create, :reset_password]
   before_action :find_user, except: %i[create index reset_password]
 
   # GET /users
