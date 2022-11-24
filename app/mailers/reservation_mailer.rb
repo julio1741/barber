14517e95a599b72d7c6b5ad8699bd551b9dc0abe
Julio Baptista <julio.baptista@routinguc.com>
2022-11-23 22:00:02 -0300

    Change subject

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -21,6 +21,6 @@ class ReservationMailer < ApplicationMailer
     @work_day = @reservation.work_day
     @block_time = @reservation.block_time
     app_host = ENV.fetch('APP_HOST', nil)
-    mail(to: email, subject: 'Gracias por reservar!') if email.present?
+    mail(to: email, subject: 'Reserva cancelada!') if email.present?
   end
 end
